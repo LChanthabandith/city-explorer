14923ca7b33963525df8c738c778b5f57f261dad
Lavieng <LChanthabandith@gmail.com>
2023-08-05 02:03:30 -0500

    form and API request

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Card from 'react-bootstrap/Card';
 
-function App() {
+const CityForm = () => {
+  const [city, setCity] = useState('');
+  const [location, setLocation] = useState(null);
+
+  const getLocation = async city => {
+    const response = await axios.get(`https://us1.locationiq.com/v1/search.php`, {
+      params: {
+        key: process.env.REACT_APP_LOCATIONIQ_API_KEY,
+        q: city,
+        format: 'json',
+      },
+    });
+    return response.data[0];
+  };
+
+  const handleSubmit = async event => {
+    event.preventDefault();
+    const locationData = await getLocation(city);
+    setLocation(locationData);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group>
+          <Form.Label>City</Form.Label>
+          <Form.Control
+            type="text"
+            value={city}
+            onChange={e => setCity(e.target.value)}
+            placeholder="Enter city"
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Explore!
+        </Button>
+      </Form>
 
-export default App;
+      {location && (
+        <Card style={{ width: '18rem' }}>
+          <Card.Body>
+            <Card.Title>{location.display_name}</Card.Title>
+            <Card.Text>
+              Latitude: {location.lat}
+              <br />
+              Longitude: {location.lon}
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      )}
+    </>
+  );
+};
+
+export default CityForm;
+

32646ba347ccf7433719af58af1f58ab1c66a734
Lavieng <LChanthabandith@gmail.com>
2023-08-20 03:58:02 -0500

    fixing files and using new apis to ensure functionility

--- src/App.js
+++ src/App.js
@@ -6,8 +6,11 @@ import Card from 'react-bootstrap/Card';
 import Image from 'react-bootstrap/Image';
 import Alert from 'react-bootstrap/Alert';
 
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:3001';
+
 const CityForm = () => {
     const [city, setCity] = useState('');
+    const [loading, setLoading] = useState(false);
     const [location, setLocation] = useState(null);
     const [mapUrl, setMapUrl] = useState(null);
     const [error, setError] = useState(null);
@@ -16,27 +19,30 @@ const CityForm = () => {
 
     const getWeather = async (city) => {
       try {
-          const response = await axios.get('http://localhost:3001/weather', {
-              params: {
-                  city: city
-              }
-          });
+          const API_KEY = "86de4b6eda4fb40d873a7b997839161f";
+          const ENDPOINT = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${API_KEY}&units=imperial`;
+          
+          const response = await axios.get(ENDPOINT);
+
           if (response.status === 200 && response.data) {
-              setWeather([response.data]);
+              setWeather([{
+                  description: response.data.weather[0].description,
+                  temperature: `${response.data.main.temp}°F`
+              }]);
           } else {
               console.error("Weather data not found in response.");
           }
       } catch (error) {
-          console.error("Error fetching weather data:", error?.response?.data);
+          console.error("Error fetching weather data:", error?.response?.data || "Unknown error");
           setError({
-              status: error?.response?.status,
-              message: error?.response?.data?.message || 'Something went wrong'
+              status: error?.response?.status || "Error",
+              message: error?.response?.data?.message || 'Something went wrong fetching the weather.'
           });
       }
-    };
-  
-  
+  };
+
     const getLocation = async city => {
+        setLoading(true);
         try {
             const response = await axios.get(`https://us1.locationiq.com/v1/search.php`, {
                 params: {
@@ -52,6 +58,8 @@ const CityForm = () => {
                 message: error.response.data.error,
             });
             return null;
+        } finally {
+            setLoading(false);
         }
     };
 
@@ -59,104 +67,115 @@ const CityForm = () => {
         return `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${lat},${lon}&zoom=10`;
     }
 
-    const getMovies = async city => {
+    const getMovies = async () => {
       try {
-          const response = await axios.get('http://localhost:3001/movies', {
-              params: {
-                  city: city
-              }
-          });
-          setMovies(response.data);
+          const API_KEY = "c1f4456f";
+          const currentYear = new Date().getFullYear();
+          const ENDPOINT = `http://www.omdbapi.com/?s=${currentYear}&apikey=${API_KEY}&type=movie&y=${currentYear}`;
+  
+          const response = await axios.get(ENDPOINT);
+  
+          if (response.data.Response === "True" && response.data.Search) {
+              setMovies(response.data.Search);
+          } else {
+              console.error("Movies data not found in response.");
+          }
       } catch (error) {
-          console.error('Error fetching movie data:', error);
-      }
-  };
-
-    const handleSubmit = async event => {
-      event.preventDefault();
-      setError(null);
-
-      if (city.trim() === '') {
+          console.error("Error fetching movie data:", error?.response?.data || "Unknown error");
           setError({
-              status: 'Input Error',
-              message: 'Please enter a valid city name.'
+              status: error?.response?.status || "Error",
+              message: error?.response?.data?.Error || 'Something went wrong fetching the movies.'
           });
-          return;
-      }
-
-      const locationData = await getLocation(city);
-      if (locationData) {
-          setLocation(locationData);
-          const mapUrl = getMap(locationData.lat, locationData.lon);
-          setMapUrl(mapUrl);
-          await getWeather(city);
-          await getMovies(city);
       }
   };
+  
+  
 
-  return (
-    <>
-        <Form onSubmit={handleSubmit}>
-            <Form.Group>
-                <Form.Label>City</Form.Label>
-                <Form.Control
-                    type="text"
-                    value={city}
-                    onChange={e => setCity(e.target.value)}
-                    placeholder="Enter city"
-                />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-                Explore!
-            </Button>
-        </Form>
+  const handleSubmit = async event => {
+    event.preventDefault();
+    setError(null);
 
-        {error && (
-            <Alert variant="danger">
-                Error {error.status}: {error.message}
-            </Alert>
-        )}
+    if (city.trim() === '') {
+        setError({
+            status: 'Input Error',
+            message: 'Please enter a valid city name.'
+        });
+        return;
+    }
 
-        {location && (
-            <Card style={{ width: '18rem', marginTop: '20px' }}>
-                <Card.Body>
-                    <Card.Title>{location.display_name}</Card.Title>
-                    <Card.Text>
-                        Latitude: {location.lat}
-                        <br />
-                        Longitude: {location.lon}
-                    </Card.Text>
-                    {mapUrl && <Image src={mapUrl} />}
-                </Card.Body>
-            </Card>
-        )}
-    
-    {weather.map((day, index) => (
-        <div key={index}>
-            Date: Today
-            <br />
-            Description: {day.description}
-            <br />
-            Temperature: {day.temperature}
-        </div>
-    ))}
+    const locationData = await getLocation(city);
+    if (locationData) {
+        setLocation(locationData);
+        const mapUrl = getMap(locationData.lat, locationData.lon);
+        setMapUrl(mapUrl);
+        await getWeather(city);
+        await getMovies(city);
+    }
+};
 
-        {movies && movies.map((movie, index) => (
-            <Card key={index} style={{ width: '18rem', marginTop: '20px' }}>
-                <Card.Body>
-                    <Card.Title>{movie.title}</Card.Title>
-                    <Card.Text>
-                        Overview: {movie.overview}
-                        <br />
-                        Release Date: {movie.release_date}
-                        <br />
-                        Rating: {movie.vote_average}
-                    </Card.Text>
-                    <Image src={movie.image_url} />
-                </Card.Body>
-            </Card>
-        ))}
-    </>
+return (
+  <>
+      <Form onSubmit={handleSubmit}>
+          <Form.Group>
+              <Form.Label>City</Form.Label>
+              <Form.Control
+                  type="text"
+                  value={city}
+                  onChange={e => setCity(e.target.value)}
+                  placeholder="Enter city"
+              />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+              Explore!
+          </Button>
+      </Form>
+
+      {loading && <p>Loading...</p>}
+
+      {error && (
+          <Alert variant="danger">
+              Error {error.status}: {error.message}
+          </Alert>
+      )}
+
+      {location && (
+          <Card style={{ width: '18rem', marginTop: '20px' }}>
+              <Card.Body>
+                  <Card.Title>{location.display_name}</Card.Title>
+                  <Card.Text>
+                      Latitude: {location.lat}
+                      <br />
+                      Longitude: {location.lon}
+                  </Card.Text>
+                  {mapUrl && <Image src={mapUrl} />}
+              </Card.Body>
+          </Card>
+      )}
+
+      {weather && weather.map((day, index) => (
+          <div key={index}>
+              Date: Today
+              <br />
+              Description: {day.description ? day.description : "No description available."}
+              <br />
+              Temperature: {day.temperature ? day.temperature : "No temperature available."}
+          </div>
+      ))}
+
+{movies && movies.map((movie, index) => (
+    <Card key={index} style={{ width: '18rem', marginTop: '20px' }}>
+        <Card.Body>
+            <Card.Title>{movie.Title}</Card.Title>
+            <Card.Text>
+                Year: {movie.Year}
+                <br />
+            </Card.Text>
+            <Image src={movie.Poster} />
+        </Card.Body>
+    </Card>
+))}
+
+  </>
 );
 };
 
